e74a86e5a90f21219000cb8ef026f30533bbb489
qicqock <31722579+qicqock@users.noreply.github.com>
2020-07-16 02:27:24 +0900

    200716

--- baek/1003.cpp
+++ baek/1003.cpp
@@ -6,23 +6,48 @@ using namespace std;
 
 int count0 = 0;
 int count1 = 0; 
+int arr[41];
+vector<pair<pair<int,int>,int>> v(41);
 
-pair<int,int> fibonacci(int n){
-    int
+int fibonacci(int n){
+    if (n == 0){
+        v[n].first.first = 0;
+        v[n].first.second = 1;
+        return 0;
+    } else if (n == 1){
+        v[n].first.first = 1;
+        v[n].second = 1;
+        return 1;
+    } else {
+        if (v[n].first.first != 0){
+            return v[n].first.first;
+        }
+        else {
+            if (v[n - 1].first.first == 0){
+                v[n - 1].first.first = fibonacci(n - 1);
+            }
+            if (v[n - 2].first.first == 0){
+                v[n - 2].first.first = fibonacci(n - 2);
+            }
+            v[n].first.second = v[n- 1].first.second + v[n - 2].first.second;
+            v[n].second = v[n - 1].second + v[n - 2].second;
+            
+            return v[n - 1].first.first + v[n - 2].first.first;
+        }
+    }
 }
+
 
 int main(void){
     iostream::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     int T,N;
-    cin >> T;
+    cin >> T; 
     for (int i = 0;i < T; i++){
         cin >> N; 
-        fibonacci(N);
-        cout << count0 << " " << count1 << "\n";
-        count0 = 0;
-        count1 = 0;    
+        int a = fibonacci(N);
+        cout << v[N].first.second << " " << v[N].second << "\n";    
     }
     return 0;
 }
