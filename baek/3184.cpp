1fc445c71f45e0ecd3bdb5f4919cb8716a470340
qicqock <qicqockqvgiq@gmail.com>
2020-07-18 19:48:40 +0900

    200718

--- baek/3184.cpp
+++ baek/3184.cpp
@@ -9,43 +9,49 @@ string arr[251];
 bool check[251][251];
 int sumsheep = 0;
 int sumwolf = 0;
+ int sheep = 0;
+int wolf = 0;
 
-int backyard(int x,int y){
-    check[x][y] = true;
-    int sheep = 0;
-    int wolf = 0;
+void backyard(int x,int y){
     if (arr[x][y] == 'v'){
+        cout << "wolf";
         wolf++;
-        arr[x][y] == '#';
+        arr[x][y] = '#';
     }
     if (arr[x][y] == 'o'){
+        cout << "sheep";
         sheep++;
-        arr[x][y] == '#';
+        arr[x][y] = '#';
     }
-    if (((0 < x) && (arr[x - 1][y] != '#')) && (arr[x][y] == '.')){
+    if (((0 < x) && (arr[x - 1][y] != '#')) && (arr[x][y] != '#')){
+        cout << "1";
         if (!check[x-1,y]){
-            cout << "**1";
+            cout << "1";
             return backyard(x - 1,y);
         }
     } 
-    if (((x <  - 1) && (arr[x + 1][y] != '#')) && (arr[x][y] == '.')){
+    if (((x <  - 1) && (arr[x + 1][y] != '#')) && (arr[x][y] != '#')){
+        cout << "2";
         if (!check[x + 1,y]){
-           cout << "**2";
+            cout << "2";
             return backyard(x + 1,y);
         }
     } 
-    if (((0 < y) && (arr[x][y - 1] != '#')) && (arr[x][y] == '.')){
+    if (((0 < y) && (arr[x][y - 1] != '#')) && (arr[x][y] != '#')){
+        cout << "3";
         if (!check[x,y - 1]){
-            cout << "**3";
+            cout << "3";
             return backyard(x,y - 1);
         }
     } 
-    if (((y < arr[x].size() - 1) && (arr[x][y + 1] != '#')) && (arr[x][y] == '.')){
+    if (((y < arr[x].size() - 1) && (arr[x][y + 1] != '#')) && (arr[x][y] != '#')){
+        cout << "4";
         if (!check[x,y + 1]){
-            cout << "**4";
+            cout << "4";
             return backyard(x,y + 1);
         }
     } 
+
 }
 
 
@@ -58,23 +64,25 @@ int main(void){
     for (int i = 0;i < R;i++){
         cin >> arr[i];
     }
-    
+
     for (int i = 0; i < R; i++){
         for (int j = 0 ; j < C ; j++){
             if (arr[i][j] == '#'){
-                check[i][j] == true;
-                continue;
-            }
-            else {
-                backyard(i,j);
+                check[i][j] = true;
             }
         }
     }
 
-    cout << sheep << " " << wolf << " \n";
+    for (int i = 0; i < R; i++){
+        for (int j = 0 ; j < C ; j++){
+            if (!check[i][j]){
+                check[i][j] = true;
+                backyard(i,j);
+        }
+    }
 
-
-
+    //cout << sheep << " " << wolf << " \n";
 
     return 0;
+    }
 }
